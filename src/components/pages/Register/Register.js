040b45f77fe49f6d413ddc5eb9f8c96c5a61a29c
DelowarHossain69 <daloar2400@gmail.com>
2022-05-16 02:05:10 -0700

    register page designed

---
+++ src/components/pages/Register/Register.js
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
+
+const Register = () => {
+  const { register, handleSubmit, reset } = useForm();
+  const onSubmit = (data) => {};
+
+  return (
+    <div
+      className="container mx-auto my-5 d-flex align-items-center justify-content-center"
+      style={{ height: "80vh" }}
+    >
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="mx-auto p-4 rounded"
+        style={{ width: "400px", background: "#ddd" }}
+      >
+        <div className="text-center">
+          <img src="https://i.ibb.co/nbcYJ6P/logo-red.png" className="w-50" />
+          <h3 className="my-3">Please Register</h3>
+        </div>
+
+        <input
+          className="w-100 mb-3 p-2"
+          {...register("displayName")}
+          placeholder="Enter your name"
+          type="text"
+          required
+        />
+
+        <input
+          className="w-100 mb-3 p-2"
+          {...register("email")}
+          placeholder="Enter your email"
+          type="email"
+          required
+        />
+
+        <input
+          className="w-100 mb-3 p-2"
+          {...register("password")}
+          placeholder="Password"
+          type="text"
+          required
+        />
+
+        <input
+          className="w-100 mb-3 p-2"
+          {...register("confirmPassword")}
+          placeholder="Confirm password"
+          type="text"
+          required
+        />
+
+        <input
+          className="w-100 mb-3 p-2"
+          type="submit"
+          value="Register"
+        />
+
+        <p>
+          Already have an account? <Link to="/login"> Login</Link>
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
