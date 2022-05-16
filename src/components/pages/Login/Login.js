aa27741ef9931a5d55c812335840387ddc9fecf1
DelowarHossain69 <daloar2400@gmail.com>
2022-05-16 01:39:03 -0700

    login page updated

--- src/components/pages/Login/Login.js
+++ src/components/pages/Login/Login.js
@@ -3,17 +3,27 @@ import { useForm } from "react-hook-form";
 
 const Login = () => {
   const { register, handleSubmit, reset } = useForm();
-  const onSubmit = (data) => {
-    
-  };
+  const onSubmit = (data) => {};
   return (
-    <div className="container mx-auto my-5">
-      <form onSubmit={handleSubmit(onSubmit)} className="w-50 mx-auto">
+    <div
+      className="container mx-auto my-5 d-flex align-items-center justify-content-center"
+      style={{ height: "60vh" }}
+    >
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="mx-auto p-4 rounded"
+        style={{ width: "400px", background: "#ddd" }}
+      >
+        <div className="text-center">
+          <img src="https://i.ibb.co/nbcYJ6P/logo-red.png" className="w-50" />
+          <h3 className="my-3">Please Login</h3>
+        </div>
+
         <input
           className="w-100 mb-3 p-2"
           {...register("email")}
           placeholder="Enter your email"
-          type="text"
+          type="email"
           required
         />
 
