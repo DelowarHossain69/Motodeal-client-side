248f0930fca52d847fbfaeb6e6347a985aba80e9
DelowarHossain69 <daloar2400@gmail.com>
2022-05-16 02:42:14 -0700

    register page updated

--- src/components/pages/Register/Register.js
+++ src/components/pages/Register/Register.js
@@ -9,60 +9,59 @@ const Register = () => {
 
   return (
     <div className="container mx-auto my-5">
-      <form
-        onSubmit={handleSubmit(onSubmit)}
+
+      <div
         className="mx-auto p-4 rounded"
         style={{ width: "400px", background: "#ddd" }}
       >
-        <div className="text-center">
-          <img src="https://i.ibb.co/nbcYJ6P/logo-red.png" className="w-50" />
-          <h3 className="my-3">Please Register</h3>
-        </div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="text-center">
+            <img src="https://i.ibb.co/nbcYJ6P/logo-red.png" className="w-50" />
+            <h3 className="my-3">Please Register</h3>
+          </div>
 
-        <input
-          className="w-100 mb-3 p-2"
-          {...register("displayName")}
-          placeholder="Enter your name"
-          type="text"
-          required
-        />
+          <input
+            className="w-100 mb-3 p-2"
+            {...register("displayName")}
+            placeholder="Enter your name"
+            type="text"
+            required
+          />
 
-        <input
-          className="w-100 mb-3 p-2"
-          {...register("email")}
-          placeholder="Enter your email"
-          type="email"
-          required
-        />
+          <input
+            className="w-100 mb-3 p-2"
+            {...register("email")}
+            placeholder="Enter your email"
+            type="email"
+            required
+          />
 
-        <input
-          className="w-100 mb-3 p-2"
-          {...register("password")}
-          placeholder="Password"
-          type="text"
-          required
-        />
+          <input
+            className="w-100 mb-3 p-2"
+            {...register("password")}
+            placeholder="Password"
+            type="text"
+            required
+          />
 
-        <input
-          className="w-100 mb-3 p-2"
-          {...register("confirmPassword")}
-          placeholder="Confirm password"
-          type="text"
-          required
-        />
+          <input
+            className="w-100 mb-3 p-2"
+            {...register("confirmPassword")}
+            placeholder="Confirm password"
+            type="text"
+            required
+          />
 
-        <input
-          className="w-100 mb-3 p-2"
-          type="submit"
-          value="Register"
-        />
+          <input className="w-100 mb-3 p-2" type="submit" value="Register" />
 
-        <p>
-          Already have an account? <Link to="/login"> Login</Link>
-        </p>
+          <p>
+            Already have an account? <Link to="/login"> Login</Link>
+          </p>
+        </form>
 
-        <LoginWith/>
-      </form>
+        <LoginWith />
+      </div>
+      
     </div>
   );
 };
